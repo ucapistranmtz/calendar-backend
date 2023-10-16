675ab417c1afc5231b446cef29551a627a949a2c
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-16 17:06:44 -0500

    lesson 378

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,21 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const registerUser = (req, res = response) => {
   const { name, email, password } = req.body;
-  res.status = 200;
-  res.json({
+
+  const errors = validationResult(req);
+
+  console.log(errors);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
+  res.status(201).json({
+    ok: false,
     ok: true,
     msg: 'register',
     name,
@@ -14,6 +26,15 @@ const registerUser = (req, res = response) => {
 
 const loginUser = (req, res = response) => {
   const { email, password } = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+
   res.status = 200;
   res.json({
     ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,29 @@
 const { Router } = require('express');
 const { registerUser, loginUser, renewToken } = require('../controllers/auth');
+const { check } = require('express-validator');
 const router = Router();
 
-router.post('/new', registerUser);
+router.post(
+  '/new',
+  [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'email is required').isEmail(),
+    check('password', 'password should have minimun 6 characters').isLength({
+      min: 6,
+    }),
+  ],
+  registerUser,
+);
 
-router.post('/', loginUser);
+router.post(
+  '/',
+
+  check('email', 'email is required').isEmail(),
+  check('password', 'password should have minimun 6 characters').isLength({
+    min: 6,
+  }),
+  loginUser,
+);
 
 router.get('/renew', renewToken);
 
