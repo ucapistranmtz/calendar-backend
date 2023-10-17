08824036ac37e05d033bb2f263c3501f211229a1
Ulises Capistran <ucapistranmtz@gmail.com>
2023-10-17 07:15:18 -0500

    lesson 385 password validation bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,16 +40,40 @@ const registerUser = async (req, res = response) => {
   }
 };
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
   const { email, password } = req.body;
 
-  res.status = 200;
-  res.json({
-    ok: true,
-    msg: 'login',
-    email,
-    password,
-  });
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(400).json({
+        ok: false,
+        msg: 'user does not exists',
+      });
+    }
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      res.status(400).json({
+        ok: false,
+        msg: 'Invalid password',
+      });
+    }
+
+    res.status = 200;
+    res.json({
+      ok: true,
+      uid: user.id,
+      name: user.name,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      ok: false,
+      error: 'please contact the administrator',
+    });
+  }
 };
 
 const renewToken = (req, res = response) => {
